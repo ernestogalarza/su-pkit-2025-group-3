59c066f3ed3a0aeff14c16466d6a9cbd0b95034d
ernesto galarza <ernestoagc87@gmail.com>
2025-10-29 14:08:53 +0100

    improve environment car scene

--- Assets/Scripts/SteeringWheelManager.cs
+++ Assets/Scripts/SteeringWheelManager.cs
@@ -34,8 +34,8 @@ public class SteeringWheelManager : MonoBehaviour
         throttle = controls.Vehicle.Throttle.ReadValue<float>();
         brake = controls.Vehicle.Brake.ReadValue<float>();
        // Debug.Log($"wheel//: {wheel:F2}");
-           //  Debug.Log($"brake//: {brake:F2}");
-     //   Debug.Log($"throttle: {throttle:F2}");
+        //     Debug.Log($"brake//: {brake:F2}");
+        Debug.Log($"throttle: {throttle:F2}");
         // speedText.text = throttle.ToString();
     }
 
